7993e5a320feedb8d52565672ee2cc0afcf0af07
AlanF20 <alanif@hotmail.com>
2021-09-13 09:58:11 -0500

    Ciclo do while java

--- proyectosJava/ejemplo1/src/App.java
+++ proyectosJava/ejemplo1/src/App.java
@@ -1,15 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] miArreglo;
-        int a=1,b=2,c=10,s;
+        int a=1,b=2,c=10,s,i=0;
         System.out.println(a);
         System.out.println(b);
-        for(int i=0; i<c;i++){
+        do{
             s=a+b;
             System.out.println(s);
             a=b;
             b=s;
-        }
+            i++;
+        }while(i<c);
 
     }
 }
