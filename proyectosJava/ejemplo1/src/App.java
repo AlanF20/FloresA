56314bafd75445a9107b54298409d8742afaa712
AlanF20 <alanif@hotmail.com>
2021-09-13 09:24:11 -0500

    CicloForSerie

--- proyectosJava/ejemplo1/src/App.java
+++ proyectosJava/ejemplo1/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, Mundo!");
+        int[] miArreglo;
+        int a=1,b=2,c=10,s;
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=0; i<c;i++){
+            s=a+b;
+            System.out.println(s);
+            a=b;
+            b=s;
+        }
+
     }
 }
